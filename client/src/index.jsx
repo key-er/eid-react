b7009af40881ea63fbaa032537e6b763940a72fa
key-er <email@example.com>
2018-06-25 15:49:17 -0700

    added service worker

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,7 +14,17 @@ class App extends React.Component {
       history: false,
     }
 
-    // this.username;
+
+    // registering service worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('sw.js') //client/dist is already set in webpack config
+      .then(function() {
+        console.log('Service Worker Registered!')
+        debugger;
+      })
+    }
+
+
   }
 
 

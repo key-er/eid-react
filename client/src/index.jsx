4c67474d6be8795103915b15ccfd2c8c04a23171
key-er <email@example.com>
2018-06-25 21:50:24 -0700

    moved to index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,16 +14,6 @@ class App extends React.Component {
       history: false,
     }
 
-
-    // registering service worker
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('sw.js') //client/dist is already set in webpack config
-      .then(function() {
-        console.log('Service Worker Registered!')
-      })
-    }
-
-
   }
 
 

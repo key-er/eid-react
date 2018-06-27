d3cd87c7011e91c26454b0b4b562f39798befbe5
key-er <email@example.com>
2018-06-27 01:38:07 -0700

    commented out ws code

--- client/dist/sw.js
+++ client/dist/sw.js
@@ -1,25 +1,25 @@
-self.addEventListener('install', function(e) {
-  console.log('installing')
- e.waitUntil(
-   caches.open('eid').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.css',
-       '/images/girl.png',
-       '/bundle.js',
-       '/index.js'
-     ]);
-   })
- );
-});
+// self.addEventListener('install', function(e) {
+//   console.log('installing')
+//  e.waitUntil(
+//    caches.open('eid').then(function(cache) {
+//      return cache.addAll([
+//        '/',
+//        '/index.html',
+//        '/index.css',
+//        '/images/girl.png',
+//        '/bundle.js',
+//        '/index.js'
+//      ]);
+//    })
+//  );
+// });
 
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(e) {
+//   console.log(e.request.url);
+//   console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then(function(response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });

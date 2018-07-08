1e2e17599b2fe072805bc90a59d6224e69321676
key-er <email@example.com>
2018-07-07 23:17:58 -0700

    dev test

--- server/index.js
+++ server/index.js
@@ -6,6 +6,18 @@ const utils = require('../helpers/utils.js')
 const request = require('request')
 var query = utils.query;
 
+try {
+  console.log('no exception')
+  config = require('../config.js')
+}
+catch(e) {
+  console.log('hit exception')
+  config = {
+    'client_id': process.env.github_secret.split('|')[0] ,
+    'client_secret': process.env.github_secret.split('|')[1]
+  }
+}
+
 
 
 let app = express();
@@ -17,40 +29,48 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+var client_id = config.client_id;
+var client_secret = config.client_secret;
 
+// app.get('/login', function(req, res) {
+//   res.redirect('https://github.com/login/oauth/authorize?client_id=6b9e164de8e098f9fe9c&redirect_uri=https://eid-react.herokuapp.com/auth')
+
+// })
 
 app.get('/login', function(req, res) {
-  res.redirect('https://github.com/login/oauth/authorize?client_id=6b9e164de8e098f9fe9c&redirect_uri=https://eid-react.herokuapp.com/auth')
-
+  request.get('https://github.com/login/oauth/authorize?client_id=6b9e164de8e098f9fe9c')
 })
 
+
+// app.get('https://github.com/login/oauth/authorize?client_id=6b9e164de8e098f9fe9c')
+// it would redirect to (https://eid-react.herokuapp.com/auth because defined in UI dev settings as callback) with code as https://eid-react.herokuapp.com/auth?code=bf6c7dbc9aebb8d242bc' Now we need to exchange this  code with https://github.com/login/oauth/access_token to get token in response
 
 app.get('/auth', function(req, res) {
+  console.log('********** in auth get', req.query.code)
+  if (req.query.code) {
+    let options = {
+      url: 'https://github.com/login/oauth/access_token',
+      form: {
+      client_id:client_id,
+      client_secret:client_secret,
+      code: req.query.code,
+      redirect_uri: 'https://eid-react.herokuapp.com/auth/protected',
+      // state: req.query.state
+      }
+    }
+    request.post(options, function(err, response, body) {
+      console.log(body)
+      res.send(body)
+    })
+  }
 
-  res.send(req)
-
-  // if (req.query.code) {
-  //   let options = {
-  //     url: 'https://github.com/login/oauth/access_token',
-  //     client_id: process.env.github_secret.split('|')[0],
-  //     client_secret: process.env.github_secret.split('|')[1],
-  //     code: req.query.code,
-  //     redirect_uri: 'https://eid-react.herokuapp.com/protected',
-  //     state: req.query.state
-  //   }
-  //   request.post(options, function(err, response, body) {
-  //     res.send(body)
-  //     next()
-  //   })
-  // }
-
-  // else {
-  //   res.send('stats and code condition not match')
-  // }
+  else {
+    res.send('code condition not match')
+  }
 })
 
 
-app.get('/protected', function(req, res) {
+app.get('/auth/protected', function(req, res) {
   res.send('protected resource')
 })
 
@@ -117,7 +137,7 @@ app.get('/word/:from[0-9\-]{0}/', function (req, res) {
 });
 
 
-let port = process.env.PORT || 8088;
+let port = process.env.PORT || 8089;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
